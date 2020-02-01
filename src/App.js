9017867b459f47b7a51f598ac32acccb4ba0aee5
haquezameer <haque864@gmail.com>
2020-02-01 11:03:39 +0530

    [APP-STYLE] Change app width and height to 100%

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
+
+import CardImage from './components/CardImage';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ImageURLRegex = /(jpg|png)/g;
+
+class App extends React.Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    console.log('component did mount');
+    fetch('https://www.reddit.com/r/pics/.json?jsonp=').then(res => res.json()).then(res => this.processImages(res.data));
+  }
+
+  processImages = (data) => {
+    const processedImages = data.children.filter(item => ImageURLRegex.test(item.data.thumbnail));
+    const imageList = processedImages.map(image => image.data);
+    console.log('ImageList',imageList); 
+    this.setState({data: imageList});
+  }
+  
+  render() {
+    const {data} = this.state;
+    return (
+      <div className="App">
+        <div style={{
+          display: 'flex',
+          height: '100%',
+          width: '100%',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around'
+        }}>
+          {data ? data.map(item => <CardImage title={item.title} />): 'Loading...'}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
